a0b67ae35c508b92f127a3ec3c765a5a0188ed3f
RobertPoncelet <robert.poncelet@talk21.com>
2017-07-30 17:17:50 +0100

    Added the demon (right now a headcrabless clone of the fast zombie)

--- sp/src/game/server/hl2/npc_fastzombie.cpp
+++ sp/src/game/server/hl2/npc_fastzombie.cpp
@@ -1555,7 +1555,7 @@ void CFastZombie::BecomeTorso( const Vector &vecTorsoForce, const Vector &vecLeg
 	CapabilitiesRemove( bits_CAP_MOVE_JUMP );
 	CapabilitiesRemove( bits_CAP_MOVE_CLIMB );
 
-	ReleaseHeadcrab( EyePosition(), vecLegsForce * 0.5, true, true, true );
+	ReleaseHeadcrab(EyePosition(), vecLegsForce * 0.5, true, true, true);
 
 	BaseClass::BecomeTorso( vecTorsoForce, vecLegsForce );
 }
